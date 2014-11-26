serkia/odoo

[IMP] point_of_sale: adding a security pin field on users, and checking it when changing the cashier with very basic GUI

addons/point_of_sale/static/src/js/models.js
@@ -156,7 +156,7 @@ function openerp_pos_models(instance, module){ //module is instance.point_of_sal
             }
         },{
             model:  'res.users',
-            fields: ['name','ean13'],
+            fields: ['name','pos_security_pin','ean13'],
             domain: null,
             loaded: function(self,users){ self.users = users; },
         },{

ITsolution-git/odoo-custom

Conflicts:
	addons/point_of_sale/point_of_sale_view.xml
	addons/point_of_sale/res_users.py
	addons/point_of_sale/res_users_view.xml
	addons/point_of_sale/static/src/js/models.js

addons/point_of_sale/static/src/js/models.js
@@ -166,7 +166,7 @@ function openerp_pos_models(instance, module){ //module is instance.point_of_sal
             }
         },{
             model:  'res.users',
-            fields: ['name','barcode'],
+            fields: ['name','pos_security_pin','barcode'],
             domain: null,
             loaded: function(self,users){ self.users = users; },
         },{