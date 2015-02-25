DonCDS/odoo

[IMP] hr_department: new kanban view for HR Departments. hr add a simple kanban view that is updated by the various hr modules. Each module adds statistics and links in the kanban vignette, either in a to do or to approve column.
The department kanban view can be seen as a small dashboard to have
the main data about the department displayed. Links allow the HR
manager to be redirected towards the various things to do or to
approve in its department.

addons/hr_evaluation/hr_department.py
@@ -1,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from openerp import models, fields, api
+
+
+class hr_department(models.Model):
+    _inherit = 'hr.department'
+
+    @api.multi
+    def _compute_interview_request(self):
+        Interview = self.env['hr.evaluation.interview']
+        for department in self:
+            department.interview_request_count = Interview.search_count([
+                ('user_to_review_id.department_id', '=', department.id),
+                ('state', '=', 'waiting_answer')
+            ])
+
+    @api.multi
+    def _compute_appraisal_to_start(self):
+        Evaluation = self.env['hr_evaluation.evaluation']
+        for department in self:
+            department.appraisal_to_start_count = Evaluation.search_count([
+                ('employee_id.department_id', '=', department.id),
+                ('state', '=', 'draft')
+            ])
+
+    appraisal_to_start_count = fields.Integer(
+        compute='_compute_appraisal_to_start', string='Appraisal to Start')
+    interview_request_count = fields.Integer(
+        compute='_compute_interview_request', string='Interview Request')

addons/hr_expense/hr_department.py
@@ -1,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from openerp import models, fields, api
+
+
+class hr_department(models.Model):
+    _inherit = 'hr.department'
+
+    @api.multi
+    def _compute_expense_to_approve(self):
+        expense_data = self.env['hr.expense.expense'].read_group([('department_id', 'in', self.ids), ('state', '=', 'confirm')], ['department_id'], ['department_id'])
+        result = dict((data['department_id'][0], data['department_id_count']) for data in expense_data)
+        for department in self:
+            department.expense_to_approve_count = result.get(department.id, 0)
+
+    expense_to_approve_count = fields.Integer(
+        compute='_compute_expense_to_approve', string='Expenses to Approve')

addons/hr_holidays/hr_department.py
@@ -1,0 +1,56 @@
+# -*- coding: utf-8 -*-
+import datetime
+
+from dateutil.relativedelta import relativedelta
+from openerp import models, fields, api
+from openerp.tools import DEFAULT_SERVER_DATETIME_FORMAT
+
+
+class hr_department(models.Model):
+    _inherit = 'hr.department'
+
+    @api.multi
+    def _compute_leave_count(self):
+        Holiday = self.env['hr.holidays']
+        today_date = datetime.datetime.utcnow().date()
+        today_start = today_date.strftime(DEFAULT_SERVER_DATETIME_FORMAT) # get the midnight of the current utc day
+        today_end = (today_date + relativedelta(hours=23, minutes=59, seconds=59)).strftime(DEFAULT_SERVER_DATETIME_FORMAT)
+
+        leave_data = Holiday.read_group(
+            [('department_id', 'in', self.ids),
+             ('state', '=', 'confirm'), ('type', '=', 'remove')],
+            ['department_id'], ['department_id'])
+        allocation_data = Holiday.read_group(
+            [('department_id', 'in', self.ids),
+             ('state', '=', 'confirm'), ('type', '=', 'add')],
+            ['department_id'], ['department_id'])
+        absence_data = Holiday.read_group(
+            [('department_id', 'in', self.ids), ('state', 'not in', ['cancel', 'refuse']),
+             ('date_from', '<=', today_end), ('date_to', '>=', today_start), ('type', '=', 'remove')],
+            ['department_id'], ['department_id'])
+
+        res_leave = dict((data['department_id'][0], data['department_id_count']) for data in leave_data)
+        res_allocation = dict((data['department_id'][0], data['department_id_count']) for data in allocation_data)
+        res_absence = dict((data['department_id'][0], data['department_id_count']) for data in absence_data)
+
+        for department in self:
+            department.leave_to_approve_count = res_leave.get(department.id, 0)
+            department.allocation_to_approve_count = res_allocation.get(department.id, 0)
+            department.absence_of_today = res_absence.get(department.id, 0)
+
+
+    @api.multi
+    def _compute_total_employee(self):
+        emp_data = self.env['hr.employee'].read_group([('department_id', 'in', self.ids)], ['department_id'], ['department_id'])
+        result = dict((data['department_id'][0], data['department_id_count']) for data in emp_data)
+        for department in self:
+            department.total_employee = result.get(department.id, 0)
+
+    absence_of_today = fields.Integer(
+        compute='_compute_leave_count', string='Absence by Today')
+    leave_to_approve_count = fields.Integer(
+        compute='_compute_leave_count', string='Leave to Approve')
+    allocation_to_approve_count = fields.Integer(
+        compute='_compute_leave_count', string='Allocation to Approve')
+    total_employee = fields.Integer(
+        compute='_compute_total_employee', string='Total Employee')

addons/hr_holidays/hr_holidays.py
@@ -25,14 +25,21 @@
 import calendar
 import datetime
 from datetime import date
+import logging
 import math
 import time
 from operator import attrgetter
 
+from dateutil.relativedelta import relativedelta
+import pytz
+
 from openerp.exceptions import UserError, AccessError
+from openerp import SUPERUSER_ID
 from openerp import tools
 from openerp.osv import fields, osv
 from openerp.tools.translate import _
+
+_logger = logging.getLogger(__name__)
 
 
 class hr_holidays_status(osv.osv):
@@ -240,10 +247,10 @@ class hr_holidays(osv.osv):
     _constraints = [
         (_check_date, 'You can not have 2 leaves that overlaps on same day!', ['date_from','date_to']),
         (_check_holidays, 'The number of remaining leaves is not sufficient for this leave type', ['state','number_of_days_temp'])
-    ] 
-    
+    ]
+
     _sql_constraints = [
-        ('type_value', "CHECK( (holiday_type='employee' AND employee_id IS NOT NULL) or (holiday_type='category' AND category_id IS NOT NULL))", 
+        ('type_value', "CHECK( (holiday_type='employee' AND employee_id IS NOT NULL) or (holiday_type='category' AND category_id IS NOT NULL))",
          "The employee or employee category of this request is missing. Please make sure that your user login is linked to an employee."),
         ('date_check2', "CHECK ( (type='add') OR (date_from <= date_to))", "The start date must be anterior to the end date."),
         ('date_check', "CHECK ( number_of_days_temp >= 0 )", "The number of days must be greater than 0."),
@@ -411,11 +418,11 @@ class hr_holidays(osv.osv):
                     'allday': False,
                     'state': 'open',            # to block that meeting date in the calendar
                     'class': 'confidential'
-                }   
-                #Add the partner_id (if exist) as an attendee             
+                }
+                #Add the partner_id (if exist) as an attendee
                 if record.user_id and record.user_id.partner_id:
                     meeting_vals['partner_ids'] = [(4,record.user_id.partner_id.id)]
-                    
+
                 ctx_no_email = dict(context or {}, no_email=True)
                 meeting_id = meeting_obj.create(cr, uid, meeting_vals, context=ctx_no_email)
                 self._create_resource_leave(cr, uid, [record], context=context)
@@ -584,6 +591,32 @@ class hr_employee(osv.Model):
             res[employee_id] = {'leaves_count': leaves, 'approved_leaves_count': approved_leaves}
         return res
 
+    def _absent_employee(self, cr, uid, ids, field_name, arg, context=None):
+        today_date = datetime.datetime.utcnow().date()
+        today_start = today_date.strftime(tools.DEFAULT_SERVER_DATETIME_FORMAT) # get the midnight of the current utc day
+        today_end = (today_date + relativedelta(hours=23, minutes=59, seconds=59)).strftime(tools.DEFAULT_SERVER_DATETIME_FORMAT)
+        data = self.pool['hr.holidays'].read_group(cr, uid,
+            [('employee_id', 'in', ids), ('state', 'not in', ['cancel', 'refuse']),
+             ('date_from', '<=', today_end), ('date_to', '>=', today_start), ('type', '=', 'remove')],
+            ['employee_id'], ['employee_id'], context=context)
+        result = dict.fromkeys(ids, False)
+        for d in data:
+            if d['employee_id_count'] >= 1:
+                result[d['employee_id'][0]] = True
+        return result
+
+    def _search_absent_employee(self, cr, uid, obj, name, args, context=None):
+        today_date = datetime.datetime.utcnow().date()
+        today_start = today_date.strftime(tools.DEFAULT_SERVER_DATETIME_FORMAT) # get the midnight of the current utc day
+        today_end = (today_date + relativedelta(hours=23, minutes=59, seconds=59)).strftime(tools.DEFAULT_SERVER_DATETIME_FORMAT)
+        holiday_ids = self.pool['hr.holidays'].search_read(cr, uid, [
+            ('state', 'not in', ['cancel', 'refuse']),
+            ('date_from', '<=', today_end),
+            ('date_to', '>=', today_start),
+            ('type', '=', 'remove')], ['employee_id'], context=context)
+        absent_emp_ids = [holiday['employee_id'][0] for holiday in holiday_ids if holiday['employee_id']]
+        return [('id', 'in', absent_emp_ids)]
+
     _columns = {
         'remaining_leaves': fields.function(_get_remaining_days, string='Remaining Legal Leaves', fnct_inv=_set_remaining_days, type="float", help='Total number of legal leaves allocated to this employee, change this value to create allocation/leave request. Total based on all the leave types without overriding limit.'),
         'current_leave_state': fields.function(
@@ -595,4 +628,5 @@ class hr_employee(osv.Model):
         'leave_date_to': fields.function(_get_leave_status, multi='leave_status', type='date', string='To Date'),
         'leaves_count': fields.function(_leaves_count, multi='_leaves_count', type='integer', string='Number of Leaves (current month)'),
         'approved_leaves_count': fields.function(_leaves_count, multi='_leaves_count', type='integer', string='Approved Leaves not in Payslip', help="These leaves are approved but not taken into account for payslip"),
+        'is_absent_totay': fields.function(_absent_employee, fnct_search=_search_absent_employee, type="boolean", string="Absent Today", default=False)
     }

addons/hr_recruitment/hr_department.py
@@ -1,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from openerp import models, fields, api
+
+
+class hr_department(models.Model):
+    _inherit = 'hr.department'
+
+    @api.multi
+    def _compute_new_applicant_count(self):
+        applicant_data = self.env['hr.applicant'].read_group(
+            [('department_id', 'in', self.ids), ('stage_id.sequence', '<=', '1')],
+            ['department_id'], ['department_id'])
+        result = dict((data['department_id'][0], data['department_id_count']) for data in applicant_data)
+        for department in self:
+            department.new_applicant_count = result.get(department.id, 0)
+
+    @api.multi
+    def _compute_recruitment_stats(self):
+        job_data = self.env['hr.job'].read_group(
+            [('department_id', 'in', self.ids)],
+            ['no_of_hired_employee', 'no_of_recruitment', 'department_id'], ['department_id'])
+        new_emp = dict((data['department_id'][0], data['no_of_hired_employee']) for data in job_data)
+        expected_emp = dict((data['department_id'][0], data['no_of_recruitment']) for data in job_data)
+        for department in self:
+            department.new_hired_employee = new_emp.get(department.id, 0)
+            department.expected_employee = expected_emp.get(department.id, 0)
+
+    new_applicant_count = fields.Integer(
+        compute='_compute_new_applicant_count', string='New Applicant')
+    new_hired_employee = fields.Integer(
+        compute='_compute_recruitment_stats', string='New Hired Employee')
+    expected_employee = fields.Integer(
+        compute='_compute_recruitment_stats', string='Expected Employee')

addons/hr_timesheet_sheet/hr_department.py
@@ -1,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from openerp import models, fields, api
+
+
+class hr_department(models.Model):
+    _inherit = 'hr.department'
+
+    @api.multi
+    def _compute_timesheet_to_approve(self):
+        timesheet_data = self.env['hr_timesheet_sheet.sheet'].read_group(
+            [('department_id', 'in', self.ids), ('state', '=', 'confirm')], ['department_id'], ['department_id'])
+        result = dict((data['department_id'][0], data['department_id_count']) for data in timesheet_data)
+        for department in self:
+            department.timesheet_to_approve_count = result.get(department.id, 0)
+
+    timesheet_to_approve_count = fields.Integer(
+        compute='_compute_timesheet_to_approve', string='Timesheet to Approve')

ITsolution-git/odoo-custom

addons/hr_evaluation/hr_department.py
@@ -1,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from openerp import api, fields, models
+
+
+class hr_department(models.Model):
+    _inherit = 'hr.department'
+
+    @api.multi
+    def _compute_interview_request(self):
+        Interview = self.env['hr.evaluation.interview']
+        for department in self:
+            department.interview_request_count = Interview.search_count([
+                ('user_to_review_id.department_id', '=', department.id),
+                ('state', '=', 'waiting_answer')
+            ])
+
+    @api.multi
+    def _compute_appraisal_to_start(self):
+        Evaluation = self.env['hr_evaluation.evaluation']
+        for department in self:
+            department.appraisal_to_start_count = Evaluation.search_count([
+                ('employee_id.department_id', '=', department.id),
+                ('state', '=', 'draft')
+            ])
+
+    appraisal_to_start_count = fields.Integer(
+        compute='_compute_appraisal_to_start', string='Appraisal to Start')
+    interview_request_count = fields.Integer(
+        compute='_compute_interview_request', string='Interview Request')

addons/hr_expense/hr_department.py
@@ -1,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from openerp import api, fields, models
+
+
+class hr_department(models.Model):
+    _inherit = 'hr.department'
+
+    @api.multi
+    def _compute_expense_to_approve(self):
+        expense_data = self.env['hr.expense.expense'].read_group([('department_id', 'in', self.ids), ('state', '=', 'confirm')], ['department_id'], ['department_id'])
+        result = dict((data['department_id'][0], data['department_id_count']) for data in expense_data)
+        for department in self:
+            department.expense_to_approve_count = result.get(department.id, 0)
+
+    expense_to_approve_count = fields.Integer(
+        compute='_compute_expense_to_approve', string='Expenses to Approve')

addons/hr_holidays/hr_department.py
@@ -1,0 +1,56 @@
+# -*- coding: utf-8 -*-
+import datetime
+
+from dateutil.relativedelta import relativedelta
+from openerp import api, fields, models
+from openerp.tools import DEFAULT_SERVER_DATETIME_FORMAT
+
+
+class hr_department(models.Model):
+    _inherit = 'hr.department'
+
+    @api.multi
+    def _compute_leave_count(self):
+        Holiday = self.env['hr.holidays']
+        today_date = datetime.datetime.utcnow().date()
+        today_start = today_date.strftime(DEFAULT_SERVER_DATETIME_FORMAT) # get the midnight of the current utc day
+        today_end = (today_date + relativedelta(hours=23, minutes=59, seconds=59)).strftime(DEFAULT_SERVER_DATETIME_FORMAT)
+
+        leave_data = Holiday.read_group(
+            [('department_id', 'in', self.ids),
+             ('state', '=', 'confirm'), ('type', '=', 'remove')],
+            ['department_id'], ['department_id'])
+        allocation_data = Holiday.read_group(
+            [('department_id', 'in', self.ids),
+             ('state', '=', 'confirm'), ('type', '=', 'add')],
+            ['department_id'], ['department_id'])
+        absence_data = Holiday.read_group(
+            [('department_id', 'in', self.ids), ('state', 'not in', ['cancel', 'refuse']),
+             ('date_from', '<=', today_end), ('date_to', '>=', today_start), ('type', '=', 'remove')],
+            ['department_id'], ['department_id'])
+
+        res_leave = dict((data['department_id'][0], data['department_id_count']) for data in leave_data)
+        res_allocation = dict((data['department_id'][0], data['department_id_count']) for data in allocation_data)
+        res_absence = dict((data['department_id'][0], data['department_id_count']) for data in absence_data)
+
+        for department in self:
+            department.leave_to_approve_count = res_leave.get(department.id, 0)
+            department.allocation_to_approve_count = res_allocation.get(department.id, 0)
+            department.absence_of_today = res_absence.get(department.id, 0)
+
+
+    @api.multi
+    def _compute_total_employee(self):
+        emp_data = self.env['hr.employee'].read_group([('department_id', 'in', self.ids)], ['department_id'], ['department_id'])
+        result = dict((data['department_id'][0], data['department_id_count']) for data in emp_data)
+        for department in self:
+            department.total_employee = result.get(department.id, 0)
+
+    absence_of_today = fields.Integer(
+        compute='_compute_leave_count', string='Absence by Today')
+    leave_to_approve_count = fields.Integer(
+        compute='_compute_leave_count', string='Leave to Approve')
+    allocation_to_approve_count = fields.Integer(
+        compute='_compute_leave_count', string='Allocation to Approve')
+    total_employee = fields.Integer(
+        compute='_compute_total_employee', string='Total Employee')

addons/hr_holidays/hr_holidays.py
@@ -25,14 +25,21 @@
 import calendar
 import datetime
 from datetime import date
+import logging
 import math
 import time
 from operator import attrgetter
 
+from dateutil.relativedelta import relativedelta
+import pytz
+
 from openerp.exceptions import UserError, AccessError
+from openerp import SUPERUSER_ID
 from openerp import tools
 from openerp.osv import fields, osv
 from openerp.tools.translate import _
+
+_logger = logging.getLogger(__name__)
 
 
 class hr_holidays_status(osv.osv):
@@ -236,7 +243,7 @@ class hr_holidays(osv.osv):
     ]
 
     _sql_constraints = [
-        ('type_value', "CHECK( (holiday_type='employee' AND employee_id IS NOT NULL) or (holiday_type='category' AND category_id IS NOT NULL))", 
+        ('type_value', "CHECK( (holiday_type='employee' AND employee_id IS NOT NULL) or (holiday_type='category' AND category_id IS NOT NULL))",
          "The employee or employee category of this request is missing. Please make sure that your user login is linked to an employee."),
         ('date_check2', "CHECK ( (type='add') OR (date_from <= date_to))", "The start date must be anterior to the end date."),
         ('date_check', "CHECK ( number_of_days_temp >= 0 )", "The number of days must be greater than 0."),
@@ -404,11 +411,11 @@ class hr_holidays(osv.osv):
                     'allday': False,
                     'state': 'open',            # to block that meeting date in the calendar
                     'class': 'confidential'
-                }   
-                #Add the partner_id (if exist) as an attendee             
+                }
+                #Add the partner_id (if exist) as an attendee
                 if record.user_id and record.user_id.partner_id:
                     meeting_vals['partner_ids'] = [(4,record.user_id.partner_id.id)]
-                    
+
                 ctx_no_email = dict(context or {}, no_email=True)
                 meeting_id = meeting_obj.create(cr, uid, meeting_vals, context=ctx_no_email)
                 self._create_resource_leave(cr, uid, [record], context=context)
@@ -587,6 +594,32 @@ class hr_employee(osv.Model):
             res[employee_id] = {'leaves_count': leaves, 'approved_leaves_count': approved_leaves}
         return res
 
+    def _absent_employee(self, cr, uid, ids, field_name, arg, context=None):
+        today_date = datetime.datetime.utcnow().date()
+        today_start = today_date.strftime(tools.DEFAULT_SERVER_DATETIME_FORMAT) # get the midnight of the current utc day
+        today_end = (today_date + relativedelta(hours=23, minutes=59, seconds=59)).strftime(tools.DEFAULT_SERVER_DATETIME_FORMAT)
+        data = self.pool['hr.holidays'].read_group(cr, uid,
+            [('employee_id', 'in', ids), ('state', 'not in', ['cancel', 'refuse']),
+             ('date_from', '<=', today_end), ('date_to', '>=', today_start), ('type', '=', 'remove')],
+            ['employee_id'], ['employee_id'], context=context)
+        result = dict.fromkeys(ids, False)
+        for d in data:
+            if d['employee_id_count'] >= 1:
+                result[d['employee_id'][0]] = True
+        return result
+
+    def _search_absent_employee(self, cr, uid, obj, name, args, context=None):
+        today_date = datetime.datetime.utcnow().date()
+        today_start = today_date.strftime(tools.DEFAULT_SERVER_DATETIME_FORMAT) # get the midnight of the current utc day
+        today_end = (today_date + relativedelta(hours=23, minutes=59, seconds=59)).strftime(tools.DEFAULT_SERVER_DATETIME_FORMAT)
+        holiday_ids = self.pool['hr.holidays'].search_read(cr, uid, [
+            ('state', 'not in', ['cancel', 'refuse']),
+            ('date_from', '<=', today_end),
+            ('date_to', '>=', today_start),
+            ('type', '=', 'remove')], ['employee_id'], context=context)
+        absent_employee_ids = [holiday['employee_id'][0] for holiday in holiday_ids if holiday['employee_id']]
+        return [('id', 'in', absent_employee_ids)]
+
     _columns = {
         'remaining_leaves': fields.function(_get_remaining_days, string='Remaining Legal Leaves', fnct_inv=_set_remaining_days, type="float", help='Total number of legal leaves allocated to this employee, change this value to create allocation/leave request. Total based on all the leave types without overriding limit.'),
         'current_leave_state': fields.function(
@@ -598,4 +631,5 @@ class hr_employee(osv.Model):
         'leave_date_to': fields.function(_get_leave_status, multi='leave_status', type='date', string='To Date'),
         'leaves_count': fields.function(_leaves_count, multi='_leaves_count', type='integer', string='Number of Leaves (current month)'),
         'approved_leaves_count': fields.function(_leaves_count, multi='_leaves_count', type='integer', string='Approved Leaves not in Payslip', help="These leaves are approved but not taken into account for payslip"),
+        'is_absent_totay': fields.function(_absent_employee, fnct_search=_search_absent_employee, type="boolean", string="Absent Today", default=False)
     }

addons/hr_recruitment/hr_department.py
@@ -1,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from openerp import api, fields, models
+
+
+class hr_department(models.Model):
+    _inherit = 'hr.department'
+
+    @api.multi
+    def _compute_new_applicant_count(self):
+        applicant_data = self.env['hr.applicant'].read_group(
+            [('department_id', 'in', self.ids), ('stage_id.sequence', '<=', '1')],
+            ['department_id'], ['department_id'])
+        result = dict((data['department_id'][0], data['department_id_count']) for data in applicant_data)
+        for department in self:
+            department.new_applicant_count = result.get(department.id, 0)
+
+    @api.multi
+    def _compute_recruitment_stats(self):
+        job_data = self.env['hr.job'].read_group(
+            [('department_id', 'in', self.ids)],
+            ['no_of_hired_employee', 'no_of_recruitment', 'department_id'], ['department_id'])
+        new_emp = dict((data['department_id'][0], data['no_of_hired_employee']) for data in job_data)
+        expected_emp = dict((data['department_id'][0], data['no_of_recruitment']) for data in job_data)
+        for department in self:
+            department.new_hired_employee = new_emp.get(department.id, 0)
+            department.expected_employee = expected_emp.get(department.id, 0)
+
+    new_applicant_count = fields.Integer(
+        compute='_compute_new_applicant_count', string='New Applicant')
+    new_hired_employee = fields.Integer(
+        compute='_compute_recruitment_stats', string='New Hired Employee')
+    expected_employee = fields.Integer(
+        compute='_compute_recruitment_stats', string='Expected Employee')

addons/hr_timesheet_sheet/hr_department.py
@@ -1,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from openerp import api, fields, models
+
+
+class hr_department(models.Model):
+    _inherit = 'hr.department'
+
+    @api.multi
+    def _compute_timesheet_to_approve(self):
+        timesheet_data = self.env['hr_timesheet_sheet.sheet'].read_group(
+            [('department_id', 'in', self.ids), ('state', '=', 'confirm')], ['department_id'], ['department_id'])
+        result = dict((data['department_id'][0], data['department_id_count']) for data in timesheet_data)
+        for department in self:
+            department.timesheet_to_approve_count = result.get(department.id, 0)
+
+    timesheet_to_approve_count = fields.Integer(
+        compute='_compute_timesheet_to_approve', string='Timesheet to Approve')