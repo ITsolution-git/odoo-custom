lukebranch/odoo

[REF] website_blog: update to the new module system

addons/website_blog/static/src/js/website_blog.editor.js
@@ -1,102 +1,109 @@
-(function() {
-    "use strict";
+odoo.define('website_blog.editor', ['web.ajax', 'web.core', 'web.Model', 'website.contentMenu', 'website.define', 'website.editor', 'website.snippets.editor', 'website.website'], function (require) {
+"use strict";
 
-    var website = openerp.website;
-    var _t = openerp._t;
+var ajax = require('web.ajax');
+var core = require('web.core');
+var Model = require('web.Model');
+var contentMenu = require('website.contentMenu');
+var website_define = require('website.define');
+var editor = require('website.editor');
+var snippet_editor = require('website.snippets.editor');
+var website = require('website.website');
 
-    website.EditorBarContent.include({
-        new_blog_post: function() {
-            website.prompt({
-                id: "editor_new_blog",
-                window_title: _t("New Blog Post"),
-                select: "Select Blog",
-                init: function (field) {
-                    return website.session.model('blog.blog')
-                            .call('name_search', [], { context: website.get_context() });
-                },
-            }).then(function (cat_id) {
-                document.location = '/blog/' + cat_id + '/post/new';
+
+var _t = core._t;
+
+contentMenu.EditorBarContent.include({
+    new_blog_post: function() {
+        website.prompt({
+            id: "editor_new_blog",
+            window_title: _t("New Blog Post"),
+            select: "Select Blog",
+            init: function () {
+                return new Model('blog.blog')
+                        .call('name_search', [], { context: website.get_context() });
+            },
+        }).then(function (cat_id) {
+            document.location = '/blog/' + cat_id + '/post/new';
+        });
+    },
+});
+
+website.if_dom_contains('.website_blog', function() {
+
+    editor.EditorBar.include({
+        edit: function () {
+            $('.popover').remove();
+            this._super();
+        },
+        save : function() {
+            var res = this._super();
+            if ($('.cover').length) {
+                ajax.jsonRpc("/blog/post_change_background", 'call', {
+                    'post_id' : $('#blog_post_name').attr('data-oe-id'),
+                    'image' : $('.cover').css('background-image').replace(/url\(|\)|"|'/g,''),
+                });
+            }
+            return res;
+        },
+    });
+    snippet_editor.options.many2one.include({
+        select_record: function (li) {
+            var self = this;
+            this._super(li);
+            if (this.$target.data('oe-field') === "author_id") {
+                var $nodes = $('[data-oe-model="blog.post"][data-oe-id="'+this.$target.data('oe-id')+'"][data-oe-field="author_avatar"]');
+                $nodes.each(function () {
+                    var $img = $(this).find("img");
+                    var css = window.getComputedStyle($img[0]);
+                    $img.css({ width: css.width, height: css.height });
+                    $img.attr("src", "/website/image/res.partner/"+self.ID+"/image");
+                });
+                setTimeout(function () { $nodes.removeClass('o_dirty'); },0);
+            }
+        }
+    });
+
+    snippet_editor.options.website_blog = snippet_editor.Option.extend({
+        start : function(type, value, $li) {
+            this._super();
+            this.src = this.$target.css("background-image").replace(/url\(|\)|"|'/g,'').replace(/.*none$/,'');
+            this.$image = $('<image src="'+this.src+'">');
+        },
+        clear : function(type, value, $li) {
+            if (type !== 'click') return;
+            this.src = null;
+            this.$target.css({"background-image": '', 'min-height': $(window).height()});
+            this.$image.removeAttr("src");
+        },
+        change : function(type, value, $li) {
+            if (type !== 'click') return;
+            var self = this;
+            var editor  = new editor.MediaDialog(this.$image, this.$image[0], {only_images: true});
+            editor.appendTo('body');
+            editor.on('saved', self, function (event, img) {
+                var url = self.$image.attr('src');
+                self.$target.css({"background-image": url ? 'url(' + url + ')' : "", 'min-height': $(window).height()});
             });
         },
     });
 
-    openerp.website.if_dom_contains('.website_blog', function() {
+    website_define.enable();
+    define(['summernote/summernote'], function () {
+        var enter = $.summernote.pluginEvents.enter;
+        $.summernote.pluginEvents.enter = function (event, editor, layoutInfo) {
+            enter.call(this, event, editor, layoutInfo);
 
-        website.EditorBar.include({
-            edit: function () {
-                var self = this;
-                $('.popover').remove();
-                this._super();
-                var vHeight = $(window).height();
-            },
-            save : function() {
-                var res = this._super();
-                if ($('.cover').length) {
-                    openerp.jsonRpc("/blog/post_change_background", 'call', {
-                        'post_id' : $('#blog_post_name').attr('data-oe-id'),
-                        'image' : $('.cover').css('background-image').replace(/url\(|\)|"|'/g,''),
-                    });
-                }
-                return res;
-            },
-        });
-        website.snippet.options.many2one.include({
-            select_record: function (li) {
-                var self = this;
-                this._super(li);
-                if (this.$target.data('oe-field') === "author_id") {
-                    var $nodes = $('[data-oe-model="blog.post"][data-oe-id="'+this.$target.data('oe-id')+'"][data-oe-field="author_avatar"]');
-                    $nodes.each(function () {
-                        var $img = $(this).find("img");
-                        var css = window.getComputedStyle($img[0]);
-                        $img.css({ width: css.width, height: css.height });
-                        $img.attr("src", "/website/image/res.partner/"+self.ID+"/image");
-                    });
-                    setTimeout(function () { $nodes.removeClass('o_dirty'); },0);
-                }
+            var r = $.summernote.core.range.create();
+            var node = $.summernote.core.dom.node(r.sc);
+            var $nodes = $(node).data('chatter-id') && $("p[data-chatter-id='"+$(node).data('chatter-id')+"']", layoutInfo.editable()) || $();
+            if($nodes.length > 1) {
+                $nodes.last().removeAttr('data-chatter-id');
             }
-        });
-
-        website.snippet.options.website_blog = website.snippet.Option.extend({
-            start : function(type, value, $li) {
-                this._super();
-                this.src = this.$target.css("background-image").replace(/url\(|\)|"|'/g,'').replace(/.*none$/,'');
-                this.$image = $('<image src="'+this.src+'">');
-            },
-            clear : function(type, value, $li) {
-                if (type !== 'click') return;
-                this.src = null;
-                this.$target.css({"background-image": '', 'min-height': $(window).height()});
-                this.$image.removeAttr("src");
-            },
-            change : function(type, value, $li) {
-                if (type !== 'click') return;
-                var self = this;
-                var editor  = new website.editor.MediaDialog(this.$image, this.$image[0], {only_images: true});
-                editor.appendTo('body');
-                editor.on('saved', self, function (event, img) {
-                    var url = self.$image.attr('src');
-                    self.$target.css({"background-image": url ? 'url(' + url + ')' : "", 'min-height': $(window).height()});
-                });
-            },
-        });
-
-        openerp.define.active();
-        define(['summernote/summernote'], function () {
-            var enter = $.summernote.pluginEvents.enter;
-            $.summernote.pluginEvents.enter = function (event, editor, layoutInfo) {
-                enter.call(this, event, editor, layoutInfo);
-
-                var r = $.summernote.core.range.create();
-                var node = $.summernote.core.dom.node(r.sc);
-                var $nodes = $(node).data('chatter-id') && $("p[data-chatter-id='"+$(node).data('chatter-id')+"']", layoutInfo.editable()) || $();
-                if($nodes.length > 1) {
-                    $nodes.last().removeAttr('data-chatter-id');
-                }
-            };
-        });
-        openerp.define.desactive();
-
+        };
     });
+    website_define.disable();
 
-})();
+});
+
+});

ITsolution-git/odoo-custom

addons/website_blog/static/src/js/website_blog.editor.js
@@ -1,145 +1,154 @@
-(function() {
-    "use strict";
+odoo.define('website_blog.editor', ['web.ajax', 'web.core', 'web.Model', 'website.contentMenu', 'website.define', 'website.editor', 'website.snippets.editor', 'website.website'], function (require) {
+"use strict";
 
-    var website = openerp.website;
-    var _t = openerp._t;
+var ajax = require('web.ajax');
+var core = require('web.core');
+var Model = require('web.Model');
+var contentMenu = require('website.contentMenu');
+var website_define = require('website.define');
+var editor = require('website.editor');
+var snippet_editor = require('website.snippets.editor');
+var website = require('website.website');
 
-    website.EditorBarContent.include({
-        new_blog_post: function () {
-            website.session.model('blog.blog').call('name_search', [], { context: website.get_context() })
-            .then(function (blog_ids) {
-                if (blog_ids.length == 1) {
-                    document.location = '/blog/' + blog_ids[0][0] + '/post/new';
-                } else if (blog_ids.length > 1) {
-                    website.prompt({
-                        id: "editor_new_blog",
-                        window_title: _t("New Blog Post"),
-                        select: "Select Blog",
-                        init: function (field) {
-                          return blog_ids;
-                        },
-                    }).then(function (blog_id) {
-                        document.location = '/blog/' + blog_id + '/post/new';
-                      });
-                }
-            });
+
+var _t = core._t;
+
+contentMenu.EditorBarContent.include({
+    new_blog_post: function () {
+        var model = new Model('blog.blog');
+        model.call('name_search', [], { context: website.get_context() })
+        .then(function (blog_ids) {
+            if (blog_ids.length == 1) {
+                document.location = '/blog/' + blog_ids[0][0] + '/post/new';
+            } else if (blog_ids.length > 1) {
+                website.prompt({
+                    id: "editor_new_blog",
+                    window_title: _t("New Blog Post"),
+                    select: "Select Blog",
+                    init: function (field) {
+                        return blog_ids;
+                    },
+                }).then(function (blog_id) {
+                    document.location = '/blog/' + blog_id + '/post/new';
+                });
+            }
+        });
+    },
+});
+
+website.if_dom_contains('.website_blog', function() {
+
+    editor.EditorBar.include({
+        edit: function () {
+            $('.popover').remove();
+            $('.cover_footer').off('click');
+            this._super();
+        },
+        save : function () {
+            var args = arguments;
+            var super_call = this._super;
+            var self = this;
+            if ($('#js_blogcover').length) {
+                var properties = {
+                    "background-image": $('#js_blogcover').css("background-image").replace(/"/g, ''),
+                    "background-color": $('#js_blogcover').attr("class"),
+                    "opacity": $('#js_blogcover').css("opacity"),
+                    "resize_class": $('#title').attr('class'),
+                };
+                ajax.jsonRpc("/blog/post_change_background", 'call', {
+                    'post_id' : $('h1[data-oe-expression="blog_post.name"]').attr('data-oe-id'),
+                    'cover_properties' : JSON.stringify(properties),
+                }).always(function() {
+                    super_call.apply(self, args);
+                });
+            } else {
+                super_call.apply(self, args);
+            }
         },
     });
 
-    openerp.website.if_dom_contains('.website_blog', function() {
-
-        website.EditorBar.include({
-            edit: function () {
-                var self = this;
-                $('.popover').remove();
-                $('.cover_footer').off('click');
-                this._super();
-            },
-            save : function () {
-                var args = arguments;
-                var super_call = this._super;
-                var self = this;
-                if ($('#js_blogcover').length) {
-                    var properties = {
-                        "background-image": $('#js_blogcover').css("background-image").replace(/"/g, ''),
-                        "background-color": $('#js_blogcover').attr("class"),
-                        "opacity": $('#js_blogcover').css("opacity"),
-                        "resize_class": $('#title').attr('class'),
-                    };
-                    openerp.jsonRpc("/blog/post_change_background", 'call', {
-                        'post_id' : $('h1[data-oe-expression="blog_post.name"]').attr('data-oe-id'),
-                        'cover_properties' : JSON.stringify(properties),
-                    }).always(function() {
-                        super_call.apply(self, args);
-                    });
-                } else {
-                    super_call.apply(self, args);
-                }
-            },
-        });
-
-        website.snippet.options.many2one.include({
-            select_record: function (li) {
-                var self = this;
-                this._super(li);
-                if (this.$target.data('oe-field') === "author_id") {
-                    var $nodes = $('[data-oe-model="blog.post"][data-oe-id="'+this.$target.data('oe-id')+'"][data-oe-field="author_avatar"]');
-                    $nodes.each(function () {
-                        var $img = $(this).find("img");
-                        var css = window.getComputedStyle($img[0]);
-                        $img.css({ width: css.width, height: css.height });
-                        $img.attr("src", "/website/image/res.partner/"+self.ID+"/image");
-                    });
-                    setTimeout(function () { $nodes.removeClass('o_dirty'); },0);
-                }
-            }
-        });
-
-        website.snippet.options.website_blog = website.snippet.Option.extend({
-            start : function(type, value, $li) {
-                this._super();
-                this.src = this.$target.css("background-image").replace(/url\(|\)|"|'/g,'').replace(/.*none$/,'');
-                this.$image = $('<image src="'+this.src+'">');
-            },
-            clear : function(type, value, $li) {
-                if (type !== 'click') return;
-                this.src = null;
-                this.$target.find('#js_blogcover').css({"background-image": '', "opacity": "1.0"}).attr("class", "oe_none");
-                this.$target.attr("class", "");
-                this.$image.removeAttr("src");
-            },
-            change : function(type, value, $li) {
-                if (type !== 'click') return;
-                var self = this;
-                var editor  = new website.editor.MediaDialog(this.$image, this.$image[0], {only_images: true});
-                editor.appendTo('body');
-                editor.on('saved', self, function (event, img) {
-                    var url = self.$image.attr('src');
-                    self.$target.find('#js_blogcover').css({"background-image": url ? 'url(' + url + ')' : ""});
-                    self.$target.attr("class","cover cover_full");
-                    this.$el.parent().find('.snippet-option-website_blog:not(li[data-change])').removeClass("hidden");
+    snippet_editor.options.many2one.include({
+        select_record: function (li) {
+            var self = this;
+            this._super(li);
+            if (this.$target.data('oe-field') === "author_id") {
+                var $nodes = $('[data-oe-model="blog.post"][data-oe-id="'+this.$target.data('oe-id')+'"][data-oe-field="author_avatar"]');
+                $nodes.each(function () {
+                    var $img = $(this).find("img");
+                    var css = window.getComputedStyle($img[0]);
+                    $img.css({ width: css.width, height: css.height });
+                    $img.attr("src", "/website/image/res.partner/"+self.ID+"/image");
                 });
-            },
-            cover_class : function(type, value, $li) {
-                this.$target.attr("class", (type === 'over' || type === 'click') ? value : this.class);
-            },
-            opacity : function(type, value, $li) {
-                this.$target.find('#js_blogcover').css("opacity", (type === 'over' || type === 'click') ? value : this.value);
-            },
-            bgcolor : function(type, value, $li) {
-                this.$target.find('#js_blogcover').attr("class", (type === 'over' || type === 'click') ? value : this.background);
-            },
-            set_active: function(){
-                this.background = this.$target.find('#js_blogcover').attr("class");
-                this.class = this.$target.attr('class');
-                this.value = this.$target.find('#js_blogcover').css('opacity');
-                if ( this.$target.hasClass("cover") ) {
-                    this.$el.parent().find('.snippet-option-website_blog:not(li[data-change])').removeClass("hidden");
-                } else {
-                    this.$el.parent().find('.snippet-option-website_blog:not(li[data-change])').addClass("hidden");
-                }
-                this.$el.find('li[data-bgcolor]').removeClass("active").siblings('[data-bgcolor="' + this.background + '"]').addClass("active");
-                this.$el.find('li[data-opacity]').removeClass("active").siblings('[data-opacity="' + parseFloat(this.value).toFixed(1) + '"]').addClass("active");
-                this.$el.find('li[data-cover_class]').removeClass("active").siblings('[data-cover_class="' + this.class + '"]').addClass("active");
+                setTimeout(function () { $nodes.removeClass('o_dirty'); },0);
             }
-        });
-
-        openerp.define.active();
-        define(['summernote/summernote'], function () {
-            var enter = $.summernote.pluginEvents.enter;
-            $.summernote.pluginEvents.enter = function (event, editor, layoutInfo) {
-                enter.call(this, event, editor, layoutInfo);
-
-                var r = $.summernote.core.range.create();
-                var node = $.summernote.core.dom.node(r.sc);
-                var $nodes = $(node).data('chatter-id') && $("p[data-chatter-id='"+$(node).data('chatter-id')+"']", layoutInfo.editable()) || $();
-                if($nodes.length > 1) {
-                    $nodes.last().removeAttr('data-chatter-id');
-                }
-            };
-        });
-        openerp.define.desactive();
-
+        }
     });
 
-})();
+    snippet_editor.options.website_blog = snippet_editor.Option.extend({
+        start : function(type, value, $li) {
+            this._super();
+            this.src = this.$target.css("background-image").replace(/url\(|\)|"|'/g,'').replace(/.*none$/,'');
+            this.$image = $('<image src="'+this.src+'">');
+        },
+        clear : function(type, value, $li) {
+            if (type !== 'click') return;
+            this.src = null;
+            this.$target.find('#js_blogcover').css({"background-image": '', "opacity": "1.0"}).attr("class", "oe_none");
+            this.$target.attr("class", "");
+            this.$image.removeAttr("src");
+        },
+        change : function(type, value, $li) {
+            if (type !== 'click') return;
+            var self = this;
+            var editor  = new editor.MediaDialog(this.$image, this.$image[0], {only_images: true});
+            editor.appendTo('body');
+            editor.on('saved', self, function (event, img) {
+                var url = self.$image.attr('src');
+                self.$target.find('#js_blogcover').css({"background-image": url ? 'url(' + url + ')' : ""});
+                self.$target.attr("class","cover cover_full");
+                this.$el.parent().find('.snippet-option-website_blog:not(li[data-change])').removeClass("hidden");
+            });
+        },
+        cover_class : function(type, value, $li) {
+            this.$target.attr("class", (type === 'over' || type === 'click') ? value : this.class);
+        },
+        opacity : function(type, value, $li) {
+            this.$target.find('#js_blogcover').css("opacity", (type === 'over' || type === 'click') ? value : this.value);
+        },
+        bgcolor : function(type, value, $li) {
+            this.$target.find('#js_blogcover').attr("class", (type === 'over' || type === 'click') ? value : this.background);
+        },
+        set_active: function(){
+            this.background = this.$target.find('#js_blogcover').attr("class");
+            this.class = this.$target.attr('class');
+            this.value = this.$target.find('#js_blogcover').css('opacity');
+            if ( this.$target.hasClass("cover") ) {
+              this.$el.parent().find('.snippet-option-website_blog:not(li[data-change])').removeClass("hidden");
+            } else {
+                this.$el.parent().find('.snippet-option-website_blog:not(li[data-change])').addClass("hidden");
+            }
+            this.$el.find('li[data-bgcolor]').removeClass("active").siblings('[data-bgcolor="' + this.background + '"]').addClass("active");
+            this.$el.find('li[data-opacity]').removeClass("active").siblings('[data-opacity="' + parseFloat(this.value).toFixed(1) + '"]').addClass("active");
+            this.$el.find('li[data-cover_class]').removeClass("active").siblings('[data-cover_class="' + this.class + '"]').addClass("active");
+        },
+    });
+
+    website_define.enable();
+    define(['summernote/summernote'], function () {
+        var enter = $.summernote.pluginEvents.enter;
+        $.summernote.pluginEvents.enter = function (event, editor, layoutInfo) {
+            enter.call(this, event, editor, layoutInfo);
+
+            var r = $.summernote.core.range.create();
+            var node = $.summernote.core.dom.node(r.sc);
+            var $nodes = $(node).data('chatter-id') && $("p[data-chatter-id='"+$(node).data('chatter-id')+"']", layoutInfo.editable()) || $();
+            if($nodes.length > 1) {
+                $nodes.last().removeAttr('data-chatter-id');
+            }
+        };
+    });
+    website_define.disable();
+
+});
+
+});