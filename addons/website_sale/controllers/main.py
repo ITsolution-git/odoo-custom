odoo/odoo

commit 4f91dc371c0e736dad5515d24af9b44417ba2c38

[FIX] website_sale: transaction error
When an error occurs during the transaction, the customer must receive
a message to inform him.

opw:645705

addons/website_sale/controllers/main.py
@@ -766,8 +766,8 @@ class website_sale(http.Controller):
                 message = '<p>%s</p>' % _('Your transaction is waiting confirmation.')
                 if tx.acquirer_id.post_msg:
                     message += tx.acquirer_id.post_msg
-            else:
-                message = '<p>%s</p>' % _('Your transaction is waiting confirmation.')
+            elif state == 'error':
+                message = '<p>%s</p>' % _('An error occured during the transaction.')
             validation = tx.acquirer_id.validation
 
         return {

ITsolution-git/odoo-custom

commit 1da78c5d2da50ee9dda73268b0a198085222c327

[FIX] website_sale: error in message
Error introduced by 4f91dc371c0e736dad5515d24af9b44417ba2c38

addons/website_sale/controllers/main.py
@@ -767,7 +767,7 @@ class website_sale(http.Controller):
                 if tx.acquirer_id.post_msg:
                     message += tx.acquirer_id.post_msg
             elif state == 'error':
-                message = '<p>%s</p>' % _('An error occured during the transaction.')
+                message = '<p>%s</p>' % _('An error occurred during the transaction.')
             validation = tx.acquirer_id.validation
 
         return {