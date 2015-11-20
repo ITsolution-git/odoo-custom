odoo/odoo

commit ec29736801fbd6dd5f845f1f6e7e713406ad839b

[WIP] point_of_sale: work on new payment screen

addons/point_of_sale/static/src/js/widget_base.js
@@ -1,5 +1,7 @@
 function openerp_pos_basewidget(instance, module){ //module is instance.point_of_sale
 
+    var round_pr = instance.web.round_precision
+
     // This is a base class for all Widgets in the POS. It exposes relevant data to the 
     // templates : 
     // - widget.currency : { symbol: '$' | '€' | ..., position: 'before' | 'after }
@@ -27,6 +29,12 @@ function openerp_pos_basewidget(instance, module){ //module is instance.point_of
 
             var decimals = Math.max(0,Math.ceil(Math.log(1.0 / this.currency.rounding) / Math.log(10)));
 
+            this.format_currency_no_symbol = function(amount){
+                amount = round_pr(amount,this.currency.rounding);
+                amount = amount.toFixed(decimals);
+                return amount;
+            };
+
             this.format_currency = function(amount){
                 if(typeof amount === 'number'){
                     amount = Math.round(amount*100)/100;
@@ -37,7 +45,7 @@ function openerp_pos_basewidget(instance, module){ //module is instance.point_of
                 }else{
                     return this.currency.symbol + ' ' + amount;
                 }
-            }
+            };
 
         },
         show: function(){

addons/point_of_sale/static/src/css/pos.css
@@ -932,12 +932,18 @@ td {
     overflow-y: auto;
     border-right: dashed 1px rgb(215,215,215);
 }
+.screen .left-content.pc40{
+    right: 60%;
+}
 .screen .right-content{
     position: absolute;
     right:0px; top: 64px; bottom: 0px;
     left:50%;
     overflow-x: hidden;
     overflow-y: auto;
+}
+.screen .right-content.pc60{
+    left:40%
 }
 .screen .centered-content{
     position: absolute;
@@ -985,95 +991,89 @@ td {
     position:relative;
 }
 
+
 /* b) The payment screen */
 
-
-.pos .pos-payment-container {
+.pos .payment-numpad {
     display: inline-block;
-    font-size: 16px;
-    text-align: left;
-    width: 360px;
+    width: 230px;
+    margin-top: 8px;
+    margin-right: 8px;
+    float: right;
 }
-.pos .payment-due-total {
-    text-align: center;
-    font-weight: bold;
-    font-size: 48px;
-    margin: 27px;
-    text-shadow: 0px 2px rgb(202, 202, 202);
+
+.pos .paymentlines-container {
+    padding: 16px;
+    padding-top: 0;
+    border-bottom: dashed 1px gainsboro;
+    min-height: 148px;
+}
+
+.pos .paymentlines {
+    width: 100%;
+}
+.pos .paymentlines .controls {
+    width: 40px;
+}
+.pos .paymentlines .label > * {
+    font-size: 16px;
+    padding: 8px;
+}
+.pos .paymentlines tbody{
+    background: white;
+    border-radius: 3px;
 }
 .pos .paymentline{
-    position: relative;
-    padding: 8px;
-    border-box: 3px;
-    box-sizing: border-box;
-    -moz-box-sizing: border-box;
-    border-radius: 3px;
+    font-size: 18px;
 }
-.pos .paymentline-name{
-    margin-bottom: 8px;
+.pos .paymentline.selected {
+    font-size: 22px;
+    background: #6EC89B;
+    color: white;
 }
-.pos .paymentline-input{
-    font-size: 20px;
-    font-family: Lato;
-    display: block;
-    width: 100%;
-    box-sizing: border-box;
-    -moz-box-sizing: border-box;
-    outline: none;
-    border: none;
-    padding: 6px 8px;
+.pos .paymentline.selected .edit {
     background: white;
-    color: #484848;
-    text-align: right;
+    color: #6EC89B;
+    outline: 3px blue;
+    border: solid 4px #6EC89B;
     border-radius: 3px;
-    box-shadow: 0px 2px rgba(143, 143, 143, 0.3) inset;
 }
 
-.pos .paymentline-input:focus{
-    color: rgb(130, 124, 255);
-    box-shadow: 0px 2px rgba(219, 219, 219, 0.3) inset;
-    -webkit-animation: all 250ms linear;
+.pos .paymentline > *{
+    padding: 8px;
 }
-
-.paymentline-delete {
-    width: 32px;
-    height: 32px;
-    padding: 5px;
-    text-align: center;
-    box-sizing: border-box;
-    -moz-box-sizing: border-box;
-    position: absolute;
-    bottom: 10px;
-    left: 8px;
+.pos .paymentline .delete-button{
     cursor: pointer;
+    text-align: center;
+}
+.pos .payment-buttons {
+    display: inline-block;
+    width: 300px;
+    margin: 16px;
+    margin-right: 0px;
+    float: left;
 }
 
-.pos .pos-payment-container .left-block{
-    display: inline-block;
-    width:49%;
-    margin:0;
-    padding:0;
-    text-align:left;
-}
-.pos .pos-payment-container .infoline{
-    margin-top:5px;
-    margin-bottom:5px;
+.payment-screen   .payment-buttons .button {
+    background: rgb(221, 221, 221);
+    line-height: 48px;
+    margin-bottom: 4px;
+    border-radius: 3px;
+    font-size: 16px;
     padding: 0px 8px;
-    opacity: 0.5;
+    border: solid 1px rgb(202, 202, 202);
+    cursor: pointer;
+    text-align: center;
 }
-.pos .pos-payment-container .infoline.bigger{
-    opacity: 1;
-    font-size: 20px;
+.payment-screen  .paymentlines-empty .total {
+    text-align: center;
+    padding: 24px 0px 18px;
+    font-size: 64px;
+    color: #43996E;
+    text-shadow: 0px 2px white, 0px 2px 2px rgba(0, 0, 0, 0.27);
 }
-.pos .pos-payment-container .right-block{
-    display: inline-block;
-    width:49%;
-    margin:0;
-    padding:0;
-    text-align:right;
-}
-.pos .paymentline.selected{
-    background: rgb(220,220,220);
+.payment-screen  .paymentlines-empty .message {
+    text-align: center;
 }
 
 /* c) The receipt screen */
@@ -1488,10 +1488,12 @@ td {
     font-size: 24px;
     vertical-align: top;
 }
-.splitbill-screen .paymentmethods {
+.splitbill-screen .paymentmethods,
+.payment-screen   .paymentmethods {
     margin: 16px;
 }
-.splitbill-screen .paymentmethod {
+.splitbill-screen .paymentmethod,
+.payment-screen   .paymentmethod {
     background: rgb(221, 221, 221);
     line-height: 40px;
     margin-bottom: 4px;
@@ -1501,6 +1503,14 @@ td {
     cursor: pointer;
     text-align: center;
 }
+.splitbill-screen .paymentmethod.active,
+.payment-screen   .paymentmethod.active {
+    background: #6EC89B;
+    color: white;
+    border-color: #6EC89B;
+}
+
+
 
 /*  ********* The ActionBarWidget  ********* */
 

addons/point_of_sale/static/src/js/models.js
@@ -996,11 +996,39 @@ function openerp_pos_models(instance, module){ //module is instance.point_of_sal
                 return sum + paymentLine.get_amount();
             }), 0);
         },
-        getChange: function() {
-            return this.getPaidTotal() - this.getTotalTaxIncluded();
+        getChange: function(paymentline) {
+            if (!paymentline) {
+                var change = this.getPaidTotal() - this.getTotalTaxIncluded();
+            } else {
+                var change = -this.getTotalTaxIncluded(); 
+                var lines  = this.get('paymentLines').models;
+                for (var i = 0; i < lines.length; i++) {
+                    change += lines[i].get_amount();
+                    if (lines[i] === paymentline) {
+                        break;
+                    }
+                }
+            }
+            return round_pr(Math.max(0,change), this.pos.currency.rounding);
         },
-        getDueLeft: function() {
-            return this.getTotalTaxIncluded() - this.getPaidTotal();
+        getDueLeft: function(paymentline) {
+            if (!paymentline) {
+                var due = this.getTotalTaxIncluded() - this.getPaidTotal();
+            } else {
+                var due = this.getTotalTaxIncluded();
+                var lines = this.get('paymentLines').models;
+                for (var i = 0; i < lines.length; i++) {
+                    if (lines[i] === paymentline) {
+                        break;
+                    } else {
+                        due -= lines[i].get_amount();
+                    }
+                }
+            }
+            return round_pr(Math.max(0,due), this.pos.currency.rounding);
+        },
+        isPaid: function(){
+            return this.getDueLeft() === 0;
         },
         // sets the type of receipt 'receipt'(default) or 'invoice'
         set_receipt_type: function(type){
@@ -1030,6 +1058,19 @@ function openerp_pos_models(instance, module){ //module is instance.point_of_sal
                 for(key in arguments[0]){
                     this.screen_data[key] = arguments[0][key];
                 }
+            }
+        },
+        // remove all the paymentlines with zero money in it
+        clean_empty_paymentlines: function() {
+            var lines = this.get('paymentLines').models;
+            var empty = [];
+            for ( var i = 0; i < lines.length; i++) {
+                if (!lines[i].get_amount()) {
+                    empty.push(lines[i]);
+                }
+            }
+            for ( var i = 0; i < empty.length; i++) {
+                this.removePaymentline(empty[i]);
             }
         },
         //see set_screen_data

addons/point_of_sale/static/src/js/screens.js
@@ -849,6 +849,98 @@ function openerp_pos_screens(instance, module){ //module is instance.point_of_sa
     });
 
 
+    module.NewPaymentScreenWidget = module.ScreenWidget.extend({
+        template:      'NewPaymentScreenWidget',
+        back_screen:   'product',
+        next_screen:   'receipt',
+        show_leftpane: false,
+        show_numpad:   false,
+        init: function(parent, options) {
+            this._super(parent, options);
+            this.pos.bind('change:selectedOrder',function(){
+                    this.bind_events();
+                    this.renderElement();
+                },this);
+        },
+        click_numpad: function(button){
+            console.log(button);
+        },
+        render_numpad: function() {
+            var numpad = $(QWeb.render('PaymentScreen-Numpad', { widget:this }));
+            return numpad;
+        },
+        click_delete_paymentline: function(cid){
+            var lines = this.pos.get_order().get('paymentLines').models;
+            for ( var i = 0; i < lines.length; i++ ) {
+                if (lines[i].cid === cid) {
+                    this.pos.get_order().removePaymentline(lines[i]);
+                    this.render_paymentlines();
+                    return;
+                }
+            }
+        },
+        render_paymentlines: function() {
+            var self  = this;
+            var order = this.pos.get_order();
+            var lines = order.get('paymentLines').models;
+
+            this.$('.paymentlines-container').empty();
+            var lines = $(QWeb.render('PaymentScreen-Paymentlines', { 
+                widget: this, 
+                order: order,
+                paymentlines: lines,
+            }));
+
+            lines.on('click','.delete-button',function(){
+                self.click_delete_paymentline($(this).data('cid'));
+            });
+                
+            lines.appendTo(this.$('.paymentlines-container'));
+        },
+        click_paymentmethods: function(id){
+            var cashregister = null;
+            for ( var i = 0; i < this.pos.cashregisters.length; i++ ) {
+                if ( this.pos.cashregisters[i].journal_id[0] === id ){
+                    cashregister = this.pos.cashregisters[i];
+                    break;
+                }
+            }
+            this.pos.get_order().addPaymentline( cashregister );
+            this.render_paymentlines();
+        },
+        render_paymentmethods: function() {
+            var self = this;
+            var methods = $(QWeb.render('PaymentScreen-Paymentmethods', { widget:this }));
+                methods.on('click','.paymentmethod',function(){
+                    self.click_paymentmethods($(this).data('id'));
+                });
+            return methods;
+        },
+        renderElement: function() {
+            var self = this;
+            this._super();
+
+            var numpad = this.render_numpad();
+            numpad.appendTo(this.$('.payment-numpad'));
+
+            var methods = this.render_paymentmethods();
+            methods.appendTo(this.$('.paymentmethods-container'));
+
+            this.render_paymentlines();
+
+            this.$('.back').click(function(){
+                self.pos_widget.screen_selector.back();
+            });
+
+        },
+        show: function(){
+            this.pos.get_order().clean_empty_paymentlines();
+            this.render_paymentlines();
+            this._super();
+        },
+
+    });
+
     module.PaymentScreenWidget = module.ScreenWidget.extend({
         template: 'PaymentScreenWidget',
         back_screen: 'products',

addons/point_of_sale/static/src/js/widgets.js
@@ -1067,7 +1067,7 @@ function openerp_pos_widgets(instance, module){ //module is instance.point_of_sa
             this.receipt_screen = new module.ReceiptScreenWidget(this, {});
             this.receipt_screen.appendTo(this.$('.screens'));
 
-            this.payment_screen = new module.PaymentScreenWidget(this, {});
+            this.payment_screen = new module.NewPaymentScreenWidget(this, {});
             this.payment_screen.appendTo(this.$('.screens'));
 
             this.clientlist_screen = new module.ClientListScreenWidget(this, {});

addons/point_of_sale/static/src/xml/pos.xml
@@ -389,7 +389,146 @@
         </div>
     </t>
 
+    <t t-name="PaymentScreen-Paymentlines">
+        <t t-if="!paymentlines.length">
+            <div class='paymentlines-empty'>
+                <div class='total'>
+                    <t t-esc="widget.format_currency(order.getTotalTaxIncluded())"/>
+                </div>
+                <div class='message'>
+                    Please select a payment method. 
+                </div>
+            </div>
+        </t>
+
+        <t t-if="paymentlines.length">
+            <table class='paymentlines'>
+                <colgroup>
+                    <col class='due' />
+                    <col class='tendered' />
+                    <col class='change' />
+                    <col class='method' />
+                    <col class='controls' />
+                </colgroup>
+                <thead>
+                    <tr class='label'>
+                        <th>Due</th>
+                        <th>Tendered</th>
+                        <th>Change</th>
+                        <th>Method</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <t t-foreach='paymentlines' t-as='line'>
+                        <t t-if='line.selected'>
+                            <tr class='paymentline selected'>
+                                <td> <t t-esc='widget.format_currency_no_symbol(order.getDueLeft(line))' /> </td>
+                                <td class='edit'> <t t-esc='line.get_amount()' /> </td>
+                                <td> <t t-esc='widget.format_currency_no_symbol(order.getChange(line))' /> </td>
+                                <td> <t t-esc='line.name' /> </td>
+                                <td class='delete-button' t-att-data-cid='line.cid'> <i class='fa fa-times-circle' /> </td>
+                            </tr>
+                        </t>
+                        <t t-if='!line.selected'>
+                            <tr class='paymentline'>
+                                <td> <t t-esc='widget.format_currency_no_symbol(order.getDueLeft(line))' /> </td>
+                                <td> <t t-esc='widget.format_currency_no_symbol(line.get_amount())' /> </td>
+                                <td> <t t-esc='widget.format_currency_no_symbol(order.getChange(line))' /> </td>
+                                <td> <t t-esc='line.name' /> </td>
+                                <td class='delete-button' t-att-data-cid='line.cid'> <i class='fa fa-times-circle' /> </td>
+                            </tr>
+                        </t>
+                    </t>
+                </tbody>
+            </table>
+        </t>
+
+    </t>
+
+    <t t-name="PaymentScreen-Numpad">
+        <div class="numpad">
+            <button class="input-button number-char">1</button>
+            <button class="input-button number-char">2</button>
+            <button class="input-button number-char">3</button>
+            <button class="mode-button" data-action='currency' data-value='5' >5€</button>
+            <br />
+            <button class="input-button number-char">4</button>
+            <button class="input-button number-char">5</button>
+            <button class="input-button number-char">6</button>
+            <button class="mode-button" data-action='currency' data-value='10' >10€</button>
+            <br />
+            <button class="input-button number-char">7</button>
+            <button class="input-button number-char">8</button>
+            <button class="input-button number-char">9</button>
+            <button class="mode-button" data-action='currency' data-value='20' >20€</button>
+            <br />
+            <button class="input-button numpad-char" data-action='clear' >C</button>
+            <button class="input-button number-char">0</button>
+            <button class="input-button number-char">.</button>
+            <button class="input-button numpad-backspace" data-action='backspace' >
+                <img src="/point_of_sale/static/src/img/backspace.png" width="24" height="21" />
+            </button>
+            <br />
+        </div>
+    </t>
+
+    <t t-name="PaymentScreen-Paymentmethods">
+        <div class='paymentmethods'>
+            <t t-foreach="widget.pos.cashregisters" t-as="cashregister">
+                <div class="paymentmethod" t-att-data-id="cashregister.journal_id[0]">
+                    <t t-esc="cashregister.journal_id[1]" />
+                </div>
+            </t>
+        </div>
+    </t>
         
+    <t t-name="NewPaymentScreenWidget">
+        <div class='payment-screen screen'>
+            <div class='screen-content'>
+                <div class='top-content'>
+                    <span class='button back'>
+                        <i class='fa fa-angle-double-left'></i>
+                        Back
+                    </span>
+                    <h1>Payment</h1>
+                    <span class='button next'>
+                        Validate
+                        <i class='fa fa-angle-double-right'></i>
+                    </span>
+                </div>
+                <div class='left-content pc40 touch-scrollable scrollable-y'>
+
+                    <div class='paymentmethods-container'>
+                    </div>
+
+                </div>
+                <div class='right-content pc60 touch-scrollable scrollable-y'>
+
+                    <section class='paymentlines-container'>
+                    </section>
+
+                    <div class='payment-buttons'>
+                        <t t-if='widget.pos.config.iface_invoicing'>
+                            <div class='button js_invoice'>
+                                <i class='fa fa-file-text-o' /> Invoice
+                            </div>
+                        </t>
+                        <t t-if='widget.pos.config.iface_cashdrawer'>
+                            <div class='button js_invoice'>
+                                <i class='fa fa-archive' /> Open Cashbox
+                            </div>
+                        </t>
+                    </div>
+
+                    <section class='payment-numpad'>
+                    </section>
+
+                </div>
+            </div>
+        </div>
+
+    </t>
 
     <t t-name="PaymentScreenWidget">
         <div class="payment-screen screen touch-scrollable">

ITsolution-git/odoo-custom

commit 22d3d8e0442244f97935350903431687db4655f4

[FIX] point_of_sale: display currency amounts correctly
Displaying these amounts should be done according to what is specified
in the used language. ec29736801fbd6dd5f845f1f6e7e713406ad839b
broke this by redefining the format_currency_no_symbol function inside
of the function itself for some unknown reason.

Although we now use round_di instead of round_pr most of the time, there
shouldn't be any change in behavior because the POS manually calculates
the currency.decimals from currency.rounding.

opw-656565

addons/point_of_sale/static/src/js/widget_base.js
@@ -2,7 +2,6 @@ openerp.point_of_sale.load_basewidget = function load_basewidget(instance, modul
     "use strict";
 
     var round_di = instance.web.round_decimals;
-    var round_pr = instance.web.round_precision;
 
     // This is a base class for all Widgets in the POS. It exposes relevant data to the 
     // templates : 
@@ -39,12 +38,6 @@ openerp.point_of_sale.load_basewidget = function load_basewidget(instance, modul
             if (precision && (typeof this.pos.dp[precision]) !== undefined) {
                 decimals = this.pos.dp[precision];
             }
-
-            this.format_currency_no_symbol = function(amount){
-                amount = round_pr(amount,currency.rounding);
-                amount = amount.toFixed(decimals);
-                return amount;
-            };
 
             if (typeof amount === 'number') {
                 amount = round_di(amount,decimals).toFixed(decimals);