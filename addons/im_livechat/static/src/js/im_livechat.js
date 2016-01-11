odoo/odoo

[REM] web: Proxy.load
Not really useful at this point, if we ever go down the path of embedded
web client we can probably make the relevant controllers CORS.

addons/im_livechat/static/src/js/im_livechat.js
@@ -138,15 +138,14 @@ var LivechatButton = Widget.extend({
         });
     },
     load_qweb_template: function(){
-        var self = this;
-        var defs = [];
-        var templates = ['/mail/static/src/xml/mail_chat_common.xml', '/im_livechat/static/src/xml/im_livechat.xml'];
-        _.each(templates, function(tmpl){
-            defs.push(user_session.rpc('/web/proxy/load', {path: tmpl}).then(function(xml) {
-                QWeb.add_template(xml);
-            }));
+        return $.when(
+            $.get('/mail/static/src/xml/mail_chat_common.xml'),
+            $.get('/im_livechat/static/src/xml/im_livechat.xml')
+        ).then(function (mail_result, livechat_result) {
+            // results are triplets of [dom: XMLDocument, status: String, xhr: jqXHR]
+            QWeb.add_template(mail_result[0]);
+            QWeb.add_template(livechat_result[0]);
         });
-        return $.when.apply($, defs);
     },
     load_mail_channel: function(display_warning){
         var self = this;

ITsolution-git/odoo-custom

addons/im_livechat/static/src/js/im_livechat.js
@@ -76,16 +76,17 @@ var LivechatButton = Widget.extend({
         return this._super();
     },
 
-    load_qweb_template: function () {
-        var xml_files = ['/mail/static/src/xml/chat_window.xml',
-                         '/mail/static/src/xml/thread.xml',
-                         '/im_livechat/static/src/xml/im_livechat.xml'];
-        var defs = _.map(xml_files, function (tmpl) {
-            return session.rpc('/web/proxy/load', {path: tmpl}).then(function (xml) {
-                QWeb.add_template(xml);
-            });
+    load_qweb_template: function(){
+        return $.when(
+            $.get('/mail/static/src/xml/chat_window.xml'),
+            $.get('/mail/static/src/xml/thread.xml'),
+            $.get('/im_livechat/static/src/xml/im_livechat.xml')
+        ).then(function (chat_window, mail_thread, livechat) {
+            // results are triplets of [dom: XMLDocument, status: String, xhr: jqXHR]
+            QWeb.add_template(chat_window[0]);
+            QWeb.add_template(mail_thread[0]);
+            QWeb.add_template(livechat[0]);
         });
-        return $.when.apply($, defs);
     },
 
     open_chat: function () {