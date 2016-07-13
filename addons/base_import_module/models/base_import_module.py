TheNiceGuy/odoo

[MIG] base_import_module: Migrated to new API

addons/base_import_module/models/base_import_module.py
@@ -1,49 +1,46 @@
+# -*- coding: utf-8 -*-
 import base64
-from StringIO import StringIO
 from io import BytesIO
-from openerp.osv import osv, fields
+from odoo import api, fields, models
 
-class base_import_module(osv.TransientModel):
+
+class BaseImportModule(models.TransientModel):
     """ Import Module """
     _name = "base.import.module"
     _description = "Import Module"
 
-    _columns = {
-        'module_file': fields.binary('Module .ZIP file', required=True),
-        'state':fields.selection([('init','init'),('done','done')], 'Status', readonly=True),
-        'import_message': fields.char('Import message'),
-        'force': fields.boolean('Force init', help="Force init mode even if installed. (will update `noupdate='1'` records)"),
-    }
+    module_file = fields.Binary(string='Module .ZIP file', required=True)
+    state = fields.Selection([('init', 'init'), ('done', 'done')], string='Status', readonly=True, default='init')
+    import_message = fields.Char()
+    force = fields.Boolean(string='Force init', help="Force init mode even if installed. (will update `noupdate='1'` records)")
 
-    _defaults = {
-        'state': 'init',
-        'force': False,
-    }
-
-    def import_module(self, cr, uid, ids, context=None):
-        module_obj = self.pool.get('ir.module.module')
-        data = self.browse(cr, uid, ids[0] , context=context)
-        zip_data = base64.decodestring(data.module_file)
+    @api.multi
+    def import_module(self):
+        self.ensure_one()
+        IrModule = self.env['ir.module.module']
+        zip_data = base64.decodestring(self.module_file)
         fp = BytesIO()
         fp.write(zip_data)
-        res = module_obj.import_zipfile(cr, uid, fp, force=data.force, context=context)
-        self.write(cr, uid, ids, {'state': 'done', 'import_message': res[0]}, context=context)
-        context = dict(context, module_name=res[1])
-        # Return wizard otherwise it will close wizard and will not show result message to user. 
+        res = IrModule.import_zipfile(fp, force=self.force)
+        self.write({'state': 'done', 'import_message': res[0]})
+        context = dict(self.env.context, module_name=res[1])
+        # Return wizard otherwise it will close wizard and will not show result message to user.
         return {
             'name': 'Import Module',
             'view_type': 'form',
             'view_mode': 'form',
             'target': 'new',
-            'res_id': ids[0],
+            'res_id': self.ids,
             'res_model': 'base.import.module',
             'type': 'ir.actions.act_window',
             'context': context,
         }
 
-    def action_module_open(self, cr, uid, ids, context):
+    @api.multi
+    def action_module_open(self):
+        self.ensure_one()
         return {
-            'domain': [('name', 'in', context.get('module_name',[]))],
+            'domain': [('name', 'in', self.env.context.get('module_name', []))],
             'name': 'Modules',
             'view_type': 'form',
             'view_mode': 'tree,form',

ITsolution-git/odoo-custom

addons/base_import_module/models/base_import_module.py
@@ -1,49 +1,46 @@
+# -*- coding: utf-8 -*-
 import base64
-from StringIO import StringIO
 from io import BytesIO
-from openerp.osv import osv, fields
+from odoo import api, fields, models
 
-class base_import_module(osv.TransientModel):
+
+class BaseImportModule(models.TransientModel):
     """ Import Module """
     _name = "base.import.module"
     _description = "Import Module"
 
-    _columns = {
-        'module_file': fields.binary('Module .ZIP file', required=True),
-        'state':fields.selection([('init','init'),('done','done')], 'Status', readonly=True),
-        'import_message': fields.char('Import message'),
-        'force': fields.boolean('Force init', help="Force init mode even if installed. (will update `noupdate='1'` records)"),
-    }
+    module_file = fields.Binary(string='Module .ZIP file', required=True)
+    state = fields.Selection([('init', 'init'), ('done', 'done')], string='Status', readonly=True, default='init')
+    import_message = fields.Char()
+    force = fields.Boolean(string='Force init', help="Force init mode even if installed. (will update `noupdate='1'` records)")
 
-    _defaults = {
-        'state': 'init',
-        'force': False,
-    }
-
-    def import_module(self, cr, uid, ids, context=None):
-        module_obj = self.pool.get('ir.module.module')
-        data = self.browse(cr, uid, ids[0] , context=context)
-        zip_data = base64.decodestring(data.module_file)
+    @api.multi
+    def import_module(self):
+        self.ensure_one()
+        IrModule = self.env['ir.module.module']
+        zip_data = base64.decodestring(self.module_file)
         fp = BytesIO()
         fp.write(zip_data)
-        res = module_obj.import_zipfile(cr, uid, fp, force=data.force, context=context)
-        self.write(cr, uid, ids, {'state': 'done', 'import_message': res[0]}, context=context)
-        context = dict(context, module_name=res[1])
-        # Return wizard otherwise it will close wizard and will not show result message to user. 
+        res = IrModule.import_zipfile(fp, force=self.force)
+        self.write({'state': 'done', 'import_message': res[0]})
+        context = dict(self.env.context, module_name=res[1])
+        # Return wizard otherwise it will close wizard and will not show result message to user.
         return {
             'name': 'Import Module',
             'view_type': 'form',
             'view_mode': 'form',
             'target': 'new',
-            'res_id': ids[0],
+            'res_id': self.id,
             'res_model': 'base.import.module',
             'type': 'ir.actions.act_window',
             'context': context,
         }
 
-    def action_module_open(self, cr, uid, ids, context):
+    @api.multi
+    def action_module_open(self):
+        self.ensure_one()
         return {
-            'domain': [('name', 'in', context.get('module_name',[]))],
+            'domain': [('name', 'in', self.env.context.get('module_name', []))],
             'name': 'Modules',
             'view_type': 'form',
             'view_mode': 'tree,form',