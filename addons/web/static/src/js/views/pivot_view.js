odoo/odoo

commit 002660a9b05cf8620e1c5287a8538ac5e806d59e

[IMP] openerp, web(_*): batch views RPCs
Load the fields_view of all views of the action together. If a searchview is
required, also load its fields_view and its filters.

Use lazy-loading for fields as they are required for the Pivot and Graph
views only, and may be used by Filters and GroupBy menus of the Search view
if the users clicks on 'Add a custom Filter/Group'.

The fields are thus also loaded directly if the first view of the action is
Pivot or Graph, and otherwise, they are lazy-loaded when/if the user opens
a Pivot or Graph view, or if he wants to add a custom Filter/Group.

Some refactoring of views API as well:
- Views must now be instantiated with their fields_view
- Move code requiring fields_view from start() to init(), or willStart() for
  heavy processing.
- Remove view_type attribute from views as it was used to call
  fields_view_get.
- Remove view_id parameter to views' init as it is no longer used.
- Move duplicate code from various views' init to the View widget.
- Remove view_loading() function from views as they now follow the classical
  lifecycle of widgets (init -> willStart -> start -> destroy).
- Searchview now extends View like all other views.
- Remove useless function guard_active() as it was used for the form_view's
  action buttons to wrap handlers to ensure that they are executed only if
  the form_view is the current active view. This is not useful anymore as
  from v9, views and their control panel are appended in the DOM
  simultaneously, so one can't click on a button of a view which is not the
  current active view.

addons/web/static/src/js/views/pivot_view.js
@@ -19,10 +19,6 @@ var _t = core._t;
 var QWeb = core.qweb;
 
 var PivotView = View.extend({
-    template: 'PivotView',
-    icon: 'fa-table',
-    display_name: _lt('Pivot'),
-    view_type: 'pivot',
     events: {
         'click .oe-opened': 'on_open_header_click',
         'click .oe-closed': 'on_closed_header_click',
@@ -30,17 +26,20 @@ var PivotView = View.extend({
         'click td': 'on_cell_click',
         'click .measure-row': 'on_measure_row_click',
     },
+    display_name: _lt('Pivot'),
+    icon: 'fa-table',
+    require_fields: true,
+    template: 'PivotView',
 
-    init: function(parent, dataset, view_id, options) {
-        this._super(parent, dataset, view_id, options);
-        this.model = new Model(dataset.model, {group_by_no_leaf: true});
+    init: function() {
+        this._super.apply(this, arguments);
+        this._model = new Model(this.model, {group_by_no_leaf: true});
 
-        this.fields = {};
         this.measures = {};
         this.groupable_fields = {};
         this.ready = false; // will be ready after the first do_search
         this.data_loaded = $.Deferred();
-        this.title = options.title;
+        this.title = this.options.title || this.fields_view.arch.attrs.string;
 
         this.main_row = {
             root: undefined,
@@ -63,30 +62,72 @@ var PivotView = View.extend({
 
         this.numbering = {};
         this.widgets = [];
+
+        this.enable_linking = !this.fields_view.arch.attrs.disable_linking;
     },
     willStart: function () {
         var self = this;
-        return session.rpc('/web/pivot/check_xlwt').then(function(result) {
+
+        var load_fields = this.ViewManager.get_fields();
+        var load_xlwt = session.rpc('/web/pivot/check_xlwt').then(function(result) {
             self.xlwt_installed = result;
+        });
+
+        this.fields_view.arch.children.forEach(function (field) {
+            var name = field.attrs.name;
+            if (field.attrs.interval) {
+                name += ':' + field.attrs.interval;
+            }
+            //noinspection FallThroughInSwitchStatementJS
+            switch (field.attrs.type) {
+            case 'measure':
+                self.widgets.push(field.attrs.widget || "");
+                self.active_measures.push(name);
+                break;
+            case 'col':
+                self.initial_col_groupby.push(name);
+                break;
+            default:
+                if ('operator' in field.attrs) {
+                    self.active_measures.push(name);
+                    break;
+                }
+            case 'row':
+                self.initial_row_groupby.push(name);
+            }
+        });
+        if ((!this.active_measures.length) || this.fields_view.arch.attrs.display_quantity) {
+            this.active_measures.push('__count__');
+        }
+        return $.when(load_fields, load_xlwt, this._super()).then(function (fields) {
+            self.prepare_fields(fields);
+            // add active measures to the measure list.  This is very rarely necessary, but it
+            // can be useful if one is working with a functional field non stored, but in a
+            // model with an overrided read_group method.  In this case, the pivot view could
+            // work, and the measure should be allowed.  However, be careful if you define a
+            // measure in your pivot view: non stored functional fields will probably not work
+            // (their aggregate will always be 0).
+            _.each(self.active_measures, function (m) {
+                if (!(m in self.measures)) {
+                    self.measures[m] = self.fields[m];
+                }
+            });
         });
     },
     start: function () {
         this.$table_container = this.$('.o-pivot-table');
-
-        var load_fields = this.model.call('fields_get', [], {context: this.dataset.get_context()})
-                .then(this.prepare_fields.bind(this));
-
-        return $.when(this._super(), load_fields).then(this.render_field_selection.bind(this));
-    },
-    render_field_selection: function () {
-        var self = this;
-
-        var context = {fields: _.chain(this.groupable_fields).pairs().sortBy(function(f){return f[1].string;}).value()};
+        this.$el.toggleClass('oe_enable_linking', this.enable_linking);
+        var context = {
+            fields: _.chain(this.groupable_fields).pairs().sortBy(function(f) {
+                return f[1].string;
+            }).value(),
+        };
         this.$field_selection = this.$('.o-field-selection');
         this.$field_selection.html(QWeb.render('PivotView.FieldSelection', context));
-        core.bus.on('click', self, function () {
-            self.$field_selection.find('ul').first().hide();
+        core.bus.on('click', this, function () {
+            this.$field_selection.find('ul').first().hide();
         });
+        return this._super();
     },
     /**
      * Render the buttons according to the PivotView.buttons template and
@@ -126,49 +167,6 @@ var PivotView = View.extend({
 
             this.sidebar.appendTo($node);
         }
-    },
-    view_loading: function (fvg) {
-        var self = this;
-        this.title = this.title || fvg.arch.attrs.string;
-        this.enable_linking = !fvg.arch.attrs.disable_linking;
-        this.$el.toggleClass('oe-enable-linking', this.enable_linking);
-        fvg.arch.children.forEach(function (field) {
-            var name = field.attrs.name;
-            if (field.attrs.interval) {
-                name += ':' + field.attrs.interval;
-            }
-            //noinspection FallThroughInSwitchStatementJS
-            switch (field.attrs.type) {
-            case 'measure':
-                self.widgets.push(field.attrs.widget || "");
-                self.active_measures.push(name);
-                break;
-            case 'col':
-                self.initial_col_groupby.push(name);
-                break;
-            default:
-                if ('operator' in field.attrs) {
-                    self.active_measures.push(name);
-                    break;
-                }
-            case 'row':
-                self.initial_row_groupby.push(name);
-            }
-        });
-        if ((!self.active_measures.length) || fvg.arch.attrs.display_quantity){
-            self.active_measures.push('__count__');
-        }
-        // add active measures to the measure list.  This is very rarely necessary, but it
-        // can be useful if one is working with a functional field non stored, but in a
-        // model with an overrided read_group method.  In this case, the pivot view could
-        // work, and the measure should be allowed.  However, be careful if you define a
-        // measure in your pivot view: non stored functional fields will probably not work
-        // (their aggregate will always be 0).
-        _.each(this.active_measures, function (m) {
-            if (!(m in self.measures)) {
-                self.measures[m] = self.fields[m];
-            }
-        });
     },
     prepare_fields: function (fields) {
         var self = this,
@@ -277,15 +275,14 @@ var PivotView = View.extend({
             col_domain = this.headers[col_id].domain,
             context = _.omit(_.clone(this.context), 'group_by');
 
-        var views = [
-            [this.options.action_views_ids.list || false, 'list'],
-            [this.options.action_views_ids.form || false, 'form']
-        ];
+        var views = _.filter(this.options.action.views, function (view) {
+            return view[1] === 'form' || view[1] === 'list';
+        });
 
         return this.do_action({
             type: 'ir.actions.act_window',
             name: this.title,
-            res_model: this.model.name,
+            res_model: this.model,
             views: views,
             view_type : "list",
             view_mode : "list",
@@ -345,7 +342,7 @@ var PivotView = View.extend({
             groupbys.push([field].concat(other_groupbys.slice(0,i)));
         }
         return $.when.apply(null, groupbys.map(function (groupby) {
-            return self.model.query(fields)
+            return self._model.query(fields)
                 .filter(header.domain.length ? header.domain : self.domain)
                 .context(self.context)
                 .lazy(false)
@@ -398,7 +395,7 @@ var PivotView = View.extend({
             }
         }
         return $.when.apply(null, groupbys.map(function (groupby) {
-            return self.model.query(fields)
+            return self._model.query(fields)
                 .filter(self.domain)
                 .context(self.context)
                 .lazy(false)

addons/web/static/src/js/compatibility.js
@@ -186,8 +186,7 @@ function start_modules (modules) {
 // Monkey-patching of the ListView for backward compatibiliy of the colors and
 // fonts row's attributes, as they are deprecated in 9.0.
 ListView.include({
-    load_list: function(data) {
-        this._super(data);
+    willStart: function() {
         if (this.fields_view.arch.attrs.colors) {
             this.colors = _(this.fields_view.arch.attrs.colors.split(';')).chain()
                 .compact()
@@ -208,6 +207,8 @@ ListView.include({
                     return [font, py.parse(py.tokenize(expr)), expr];
                 }).value();
         }
+
+        return this._super(data);
     },
     /**
      * Returns the style for the provided record in the current view (from the

addons/web/static/src/js/framework/model.js
@@ -82,29 +82,31 @@ var Model = Class.extend({
      * @param {Boolean} [args.toolbar=false] get the toolbar definition
      */
     fields_view_get: function (options) {
-
         return this.call('fields_view_get', {
             view_id: options.view_id,
             view_type: options.view_type,
             context: options.context,
             toolbar: options.toolbar || false
-        }).then(function postprocess(fvg) {
-            var doc = $.parseXML(fvg.arch).documentElement;
-            fvg.arch = utils.xml_to_json(doc, (doc.nodeName.toLowerCase() !== 'kanban'));
-            if ('id' in fvg.fields) {
-                // Special case for id's
-                var id_field = fvg.fields.id;
-                id_field.original_type = id_field.type;
-                id_field.type = 'id';
-            }
-            _.each(fvg.fields, function(field) {
-                _.each(field.views || {}, function(view) {
-                    postprocess(view);
-                });
+        }).then(this.postprocess_fvg.bind(this));
+    },
+
+    postprocess_fvg: function (fvg) {
+        var self = this;
+        var doc = $.parseXML(fvg.arch).documentElement;
+        fvg.arch = utils.xml_to_json(doc, (doc.nodeName.toLowerCase() !== 'kanban'));
+        if ('id' in fvg.fields) {
+            // Special case for id's
+            var id_field = fvg.fields.id;
+            id_field.original_type = id_field.type;
+            id_field.type = 'id';
+        }
+        _.each(fvg.fields, function(field) {
+            _.each(field.views || {}, function(view) {
+                self.postprocess_fvg(view);
             });
-            return fvg;
         });
-    }
+        return fvg;
+    },
 });
 
 return Model;

addons/web/static/src/js/framework/view.js
@@ -14,10 +14,11 @@ var View = Widget.extend({
             this.do_action(action_data);
         }
     },
+    defaults: {
+        action: {},
+    },
     // name displayed in view switchers
     display_name: '',
-    // define a view type for each view to allow automatic call to fields_view_get.
-    view_type: undefined,
     // used by views that need a searchview.
     searchable: true,
     // used by views that need a searchview but don't want it to be displayed.
@@ -27,57 +28,28 @@ var View = Widget.extend({
     multi_record: true,
     // icon is the font-awesome icon to display in the view switcher
     icon: 'fa-question',
-    init: function(parent, dataset, view_id, options) {
+    // whether or not the view requires the model's fields to render itself
+    require_fields: false,
+    init: function(parent, dataset, fields_view, options) {
         this._super(parent);
         this.ViewManager = parent;
         this.dataset = dataset;
-        this.view_id = view_id;
-        this.set_default_options(options);
+        this.model = dataset.model;
+        this.fields_view = fields_view;
+        this.options = _.defaults({}, options, this.defaults);
     },
-    start: function () {
-        return this.load_view();
-    },
-    load_view: function() {
-        var self = this;
-        var view_loaded_def;
-        if (this.embedded_view) {
-            view_loaded_def = $.Deferred();
-            $.async_when().done(function() {
-                view_loaded_def.resolve(self.embedded_view);
-            });
-        } else {
-            if (! this.view_type)
-                console.warn("view_type is not defined", this);
-            view_loaded_def = this.dataset._model.fields_view_get({
-                "view_id": this.view_id,
-                "view_type": this.view_type,
-                "toolbar": !!this.options.sidebar,
-                "context": this.dataset.get_context(),
-            });
-        }
-        return this.alive(view_loaded_def).then(function(r) {
-            self.fields_view = r;
-            // add css classes that reflect the (absence of) access rights
-            self.$el.addClass('oe_view')
-                .toggleClass('oe_cannot_create', !self.is_action_enabled('create'))
-                .toggleClass('oe_cannot_edit', !self.is_action_enabled('edit'))
-                .toggleClass('oe_cannot_delete', !self.is_action_enabled('delete'));
-            return $.when(self.view_loading(r)).then(function() {
-                self.trigger('view_loaded', r);
-            });
-        });
-    },
-    view_loading: function(r) {
-    },
-    set_default_options: function(options) {
-        this.options = options || {};
-        _.defaults(this.options, {
-            // All possible views options should be defaulted here
-            $sidebar: null,
-            sidebar_id: null,
-            action: null,
-            action_views_ids: {}
-        });
+    /**
+     * Triggers event 'view_loaded'.
+     * Views extending start() must call this.super() once they are ready.
+     * @return {Deferred}
+     */
+    start: function() {
+        // add css classes that reflect the (absence of) access rights
+        this.$el.addClass('oe_view')
+            .toggleClass('oe_cannot_create', !this.is_action_enabled('create'))
+            .toggleClass('oe_cannot_edit', !this.is_action_enabled('edit'))
+            .toggleClass('oe_cannot_delete', !this.is_action_enabled('delete'));
+        return this._super().then(this.trigger.bind(this, 'view_loaded'));
     },
     /**
      * Fetches and executes the action identified by ``action_data``.
@@ -87,7 +59,7 @@ var View = Widget.extend({
      * @param {String} [action_data.special=null] special action handlers (currently: only ``'cancel'``)
      * @param {String} [action_data.type='workflow'] the action type, if present, one of ``'object'``, ``'action'`` or ``'workflow'``
      * @param {Object} [action_data.context=null] additional action context, to add to the current context
-     * @param {instance.web.DataSet} dataset a dataset object used to communicate with the server
+     * @param {DataSet} dataset a dataset object used to communicate with the server
      * @param {Object} [record_id] the identifier of the object on which the action is to be applied
      * @param {Function} on_closed callback to execute when dialog is closed or when the action does not generate any result (no new action)
      */
@@ -158,39 +130,9 @@ var View = Widget.extend({
             return dataset.exec_workflow(record_id, action_data.name).then(handler);
         }
     },
-    /**
-     * Directly set a view to use instead of calling fields_view_get. This method must
-     * be called before start(). When an embedded view is set, underlying implementations
-     * of instance.web.View must use the provided view instead of any other one.
-     *
-     * @param embedded_view A view.
-     */
-    set_embedded_view: function(embedded_view) {
-        this.embedded_view = embedded_view;
-    },
     do_show: function () {
         this._super();
         core.bus.trigger('view_shown', this);
-    },
-    is_active: function () {
-        return this.ViewManager.active_view.controller === this;
-    },
-    /**
-     * Wraps fn to only call it if the current view is the active one. If the
-     * current view is not active, doesn't call fn.
-     *
-     * fn can not return anything, as a non-call to fn can't return anything
-     * either
-     *
-     * @param {Function} fn function to wrap in the active guard
-     */
-    guard_active: function (fn) {
-        var self = this;
-        return function () {
-            if (self.is_active()) {
-                fn.apply(self, arguments);
-            }
-        };
     },
     do_push_state: function(state) {
         if (this.getParent() && this.getParent().do_push_state) {
@@ -198,7 +140,6 @@ var View = Widget.extend({
         }
     },
     do_load_state: function (state, warm) {
-
     },
     /**
      * This function should render the buttons of the view, set this.$buttons to

ITsolution-git/odoo-custom

commit 0f935146bc2af930ff7dea9af0e18c090f292200

[FIX] web: pivot cell clicking when no current list/form view
When clicking a pivot cell, an action is triggered using the cell data
and current action list view and form view. If no list view and/or form
view exists for the current viewmanager, the action chooses the default
list and/or form view.

The fallback when there is no list and/or form view was broken with
commit https://github.com/odoo/odoo/commit/002660a9b05cf8620e1c5287a8538ac5e806d59e#diff-9e64efee6e4d81d03c04c0aad259d99cL280,
when the views internal data were restructured.

Fixes #14392.

addons/web/static/src/js/views/pivot_view.js
@@ -275,9 +275,10 @@ var PivotView = View.extend({
             col_domain = this.headers[col_id].domain,
             context = _.omit(_.clone(this.context), 'group_by');
 
-        var views = _.filter(this.options.action.views, function (view) {
-            return view[1] === 'form' || view[1] === 'list';
-        });
+        var views = [
+            _find_view_info.call(this, "list"),
+            _find_view_info.call(this, "form"),
+        ];
 
         return this.do_action({
             type: 'ir.actions.act_window',
@@ -290,6 +291,12 @@ var PivotView = View.extend({
             context: context,
             domain: this.domain.concat(row_domain, col_domain),
         });
+
+        function _find_view_info(view_type) {
+            return _.find(this.options.action.views, function (view) {
+                return view[1] === view_type;
+            }) || [false, view_type];
+        }
     },
     on_measure_row_click: function (event) {
         var $target = $(event.target),