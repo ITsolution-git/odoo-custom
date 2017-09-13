facialuni/odoo

[IMP] sale_timesheet: assign task to so line
When assigning task to a SO line (through procurement),
related timesheet should become billable (according to the
product of so line), and have their revenue recomputed.

This commits also add a test.

addons/sale_timesheet/models/project.py
@@ -56,8 +56,22 @@ class Project(models.Model):
 class ProjectTask(models.Model):
     _inherit = "project.task"
 
-    procurement_id = fields.Many2one('procurement.order', 'Assign to Order', ondelete='set null', help="Procurement of the sale order line on which the timesheets should be assigned")
+    procurement_id = fields.Many2one('procurement.order', 'Sale Order Item', ondelete='set null', domain=[('is_service', '=', True)], help="Procurement of the sale order line on which the timesheets should be assigned")
     sale_line_id = fields.Many2one('sale.order.line', 'Sales Order Line', related='procurement_id.sale_line_id', store=True)
+
+    @api.multi
+    def write(self, values):
+        result = super(ProjectTask, self).write(values)
+        # reassign SO line on related timesheet lines
+        if 'procurement_id' in values:
+            so_line_id = False
+            if values.get('procurement_id'):
+                procurement = self.env['procurement.order'].sudo().browse(values['procurement_id'])
+                so_line_id = procurement.sale_line_id.id
+            self.sudo().mapped('timesheet_ids').write({
+                'so_line': so_line_id
+            })
+        return result
 
     @api.multi
     def unlink(self):

addons/sale_timesheet/tests/test_sale_service.py
@@ -2,6 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo.addons.sale_timesheet.tests.common import CommonTest
+from odoo.exceptions import UserError
 
 
 class TestSaleService(CommonTest):
@@ -87,3 +88,62 @@ class TestSaleService(CommonTest):
         })
         sale_order.action_invoice_create()
         self.assertEqual(sale_order.invoice_status, 'invoiced', 'Sale Timesheet: "invoice on delivery" timesheets should not modify the invoice_status of the so')
+
+    def test_task_so_line_assignation(self):
+        # create SO and confirm it
+        sale_order = self.env['sale.order'].create({
+            'partner_id': self.partner_usd.id,
+            'partner_invoice_id': self.partner_usd.id,
+            'partner_shipping_id': self.partner_usd.id,
+            'pricelist_id': self.pricelist_usd.id,
+        })
+        so_line_deliver_global_project = self.env['sale.order.line'].create({
+            'name': self.product_delivery_timesheet2.name,
+            'product_id': self.product_delivery_timesheet2.id,
+            'product_uom_qty': 10,
+            'product_uom': self.product_delivery_timesheet2.uom_id.id,
+            'price_unit': self.product_delivery_timesheet2.list_price,
+            'order_id': sale_order.id,
+        })
+        so_line_deliver_global_project.product_id_change()
+        sale_order.action_confirm()
+        task_serv2 = self.env['project.task'].search([('sale_line_id', '=', so_line_deliver_global_project.id)])
+        procurement = task_serv2.procurement_id
+
+        self.assertEqual(task_serv2.procurement_id.group_id, sale_order.procurement_group_id, "Procurement Group should be commun to the generated task and the SO")
+
+        # let's log some timesheets (on the project created by so_line_ordered_project_only)
+        timesheets = self.env['account.analytic.line']
+        timesheets |= self.env['account.analytic.line'].create({
+            'name': 'Test Line',
+            'project_id': task_serv2.project_id.id,
+            'task_id': task_serv2.id,
+            'unit_amount': 4,
+            'employee_id': self.employee_user.id,
+        })
+        timesheets |= self.env['account.analytic.line'].create({
+            'name': 'Test Line',
+            'project_id': task_serv2.project_id.id,
+            'task_id': task_serv2.id,
+            'unit_amount': 1,
+            'employee_id': self.employee_manager.id,
+        })
+        self.assertTrue(all([billing_type == 'billable_time' for billing_type in timesheets.mapped('timesheet_invoice_type')]), "All timesheets linked to the task should be on 'billable time'")
+        self.assertEqual(so_line_deliver_global_project.qty_to_invoice, 5, "Quantity to invoice should have been increased when logging timesheet on delivered quantities task")
+
+        # make task non billable
+        task_serv2.write({'procurement_id': False})
+        self.assertTrue(all([billing_type == 'non_billable' for billing_type in timesheets.mapped('timesheet_invoice_type')]), "Timesheet to a non billable task should be non billable too")
+
+        # make task billable again
+        task_serv2.write({'procurement_id': procurement.id})
+        self.assertTrue(all([billing_type == 'billable_time' for billing_type in timesheets.mapped('timesheet_invoice_type')]), "Timesheet to a billable time task should be billable")
+
+        # invoice SO, and validate invoice
+        invoice_id = sale_order.action_invoice_create()[0]
+        invoice = self.env['account.invoice'].browse(invoice_id)
+        invoice.action_invoice_open()
+
+        # try to update timesheets, catch error 'You cannot modify invoiced timesheet'
+        with self.assertRaises(UserError):
+            task_serv2.write({'procurement_id': False})

ITsolution-git/odoo-custom

addons/sale_timesheet/models/project.py
@@ -56,7 +56,17 @@ class Project(models.Model):
 class ProjectTask(models.Model):
     _inherit = "project.task"
 
-    sale_line_id = fields.Many2one('sale.order.line', 'Sales Order Line')
+    sale_line_id = fields.Many2one('sale.order.line', 'Sales Order Item', domain=[('is_service', '=', True)])
+
+    @api.multi
+    def write(self, values):
+        result = super(ProjectTask, self).write(values)
+        # reassign SO line on related timesheet lines
+        if 'sale_line_id' in values:
+            self.sudo().mapped('timesheet_ids').write({
+                'so_line': values['sale_line_id']
+            })
+        return result
 
     @api.multi
     def unlink(self):

addons/sale_timesheet/tests/test_sale_service.py
@@ -2,6 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo.addons.sale_timesheet.tests.common import CommonTest
+from odoo.exceptions import UserError
 
 
 class TestSaleService(CommonTest):
@@ -114,3 +115,59 @@ class TestSaleService(CommonTest):
         })
         sale_order.action_invoice_create()
         self.assertEqual(sale_order.invoice_status, 'invoiced', 'Sale Timesheet: "invoice on delivery" timesheets should not modify the invoice_status of the so')
+
+    def test_task_so_line_assignation(self):
+        # create SO and confirm it
+        sale_order = self.env['sale.order'].create({
+            'partner_id': self.partner_usd.id,
+            'partner_invoice_id': self.partner_usd.id,
+            'partner_shipping_id': self.partner_usd.id,
+            'pricelist_id': self.pricelist_usd.id,
+        })
+        so_line_deliver_global_project = self.env['sale.order.line'].create({
+            'name': self.product_delivery_timesheet2.name,
+            'product_id': self.product_delivery_timesheet2.id,
+            'product_uom_qty': 10,
+            'product_uom': self.product_delivery_timesheet2.uom_id.id,
+            'price_unit': self.product_delivery_timesheet2.list_price,
+            'order_id': sale_order.id,
+        })
+        so_line_deliver_global_project.product_id_change()
+        sale_order.action_confirm()
+        task_serv2 = self.env['project.task'].search([('sale_line_id', '=', so_line_deliver_global_project.id)])
+
+        # let's log some timesheets (on the project created by so_line_ordered_project_only)
+        timesheets = self.env['account.analytic.line']
+        timesheets |= self.env['account.analytic.line'].create({
+            'name': 'Test Line',
+            'project_id': task_serv2.project_id.id,
+            'task_id': task_serv2.id,
+            'unit_amount': 4,
+            'employee_id': self.employee_user.id,
+        })
+        timesheets |= self.env['account.analytic.line'].create({
+            'name': 'Test Line',
+            'project_id': task_serv2.project_id.id,
+            'task_id': task_serv2.id,
+            'unit_amount': 1,
+            'employee_id': self.employee_manager.id,
+        })
+        self.assertTrue(all([billing_type == 'billable_time' for billing_type in timesheets.mapped('timesheet_invoice_type')]), "All timesheets linked to the task should be on 'billable time'")
+        self.assertEqual(so_line_deliver_global_project.qty_to_invoice, 5, "Quantity to invoice should have been increased when logging timesheet on delivered quantities task")
+
+        # make task non billable
+        task_serv2.write({'sale_line_id': False})
+        self.assertTrue(all([billing_type == 'non_billable' for billing_type in timesheets.mapped('timesheet_invoice_type')]), "Timesheet to a non billable task should be non billable too")
+
+        # make task billable again
+        task_serv2.write({'sale_line_id': so_line_deliver_global_project.id})
+        self.assertTrue(all([billing_type == 'billable_time' for billing_type in timesheets.mapped('timesheet_invoice_type')]), "Timesheet to a billable time task should be billable")
+
+        # invoice SO, and validate invoice
+        invoice_id = sale_order.action_invoice_create()[0]
+        invoice = self.env['account.invoice'].browse(invoice_id)
+        invoice.action_invoice_open()
+
+        # try to update timesheets, catch error 'You cannot modify invoiced timesheet'
+        with self.assertRaises(UserError):
+            task_serv2.write({'sale_line_id': False})